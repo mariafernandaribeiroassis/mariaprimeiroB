a7fe8c5e2db5a75ad52cf77a3ede90e09ab4d0db
mariafernandaribeiroassis <144929768+mariafernandaribeiroassis@users.noreply.github.com>
2024-03-22 10:59:34 -0300

    .

--- main.js
+++ main.js
@@ -18,4 +18,4 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-06-15T23:59:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
